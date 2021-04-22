451816cfae2f0415d269375607d1c40ef4b045c7
Aliya Merali <merali4@gmail.com>
2021-04-22 15:52:55 -0600

    Setup spec file for testing simplecov

---
+++ spec/enigma_spec.rb
@@ -0,0 +1,11 @@
+require './lib/enigma'
+
+RSpec.describe Enigma do
+
+  describe '#initialize' do
+    enigma = Enigma.new
+    
+    it 'exists' do
+      expect(enigma).to be_instance_of(Enigma)
+    end
+end

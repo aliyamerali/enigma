ca305fb01260f72b06d0c292d762f1347935f4e6
Aliya Merali <merali4@gmail.com>
2021-04-23 10:26:08 -0600

    Add date_check to get_offset method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,9 +1,21 @@
+require 'Date'
+
 class Enigma
 
-
   def get_offset(date)
+    date = date_check(date)
     squared = date.to_i**2
     squared.to_s[-4..-1]
+  end
+
+  def date_check(date)
+    if date.class == String
+      date
+    elsif date.class == Date
+      date.strftime("%d%m%y")
+    else
+      "Date not in accepted format"
+    end
   end
 
   def calculate_shifts(key, date)
@@ -25,16 +37,13 @@ class Enigma
     encoder.rotate(total_shift)[0]
   end
 
-  def encrypt(message, key, date)
+  def encrypt(message, key, date=Date.today)
     shifts = calculate_shifts(key, date)
     encrypted_message = []
     message.each_char.with_index do |key_element, index|
       shift = shifts[(65 + index%4).chr.to_sym]
       encrypted_message << fwd_shift(key_element, shift)
     end
-    {encryption: encrypted_message.join,
-    key: key,
-    date: date
-    }
+    {encryption: encrypted_message.join, key: key, date: date}
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,4 +1,5 @@
 require './lib/enigma'
+require 'date'
 
 RSpec.describe Enigma do
 
@@ -10,12 +11,21 @@ RSpec.describe Enigma do
     end
   end
 
-  describe '#calculate_shifts and helper #get_offset' do
+  describe '#calculate_shifts and helper #get_offset and #date_check' do
     enigma = Enigma.new
 
     it '#get_offset takes in a date string and returns an offset' do
       date = "220421"
       expect(enigma.get_offset(date)).to eq("7241")
+    end
+
+    describe '#date_check' do
+      enigma = Enigma.new
+
+      it 'checks that a date is in the required string format, modifies it if not' do
+        expect(enigma.date_check("230421")).to eq("230421")
+        expect(enigma.date_check(Date.new(1995,8,4))).to eq("040895")
+      end
     end
 
     it '#calculate_shifts akes in key and date strings to return shift hash' do
@@ -47,18 +57,27 @@ RSpec.describe Enigma do
 
   describe '#encrypt' do
     enigma = Enigma.new
-    key = "90357"
-    date = "220421"
-    message = "Aliya Merali"
-
-    expected = {
-                encryption: "qqubqeyigfxm",
-                key: key,
-                date: date
-               }
 
     it 'returns the encoded string passed in' do
+      key = "90357"
+      date = "220421"
+      message = "Aliya Merali"
+      expected = {encryption: "qqubqeyigfxm", key: key, date: date}
+
       expect(enigma.encrypt(message, key, date)).to eq(expected)
     end
+
+    it 'uses default date of today if no date passed in' do
+      allow(Date).to receive(:today) do
+        Date.new(1995,8,4)
+      end
+
+      key = "02715"
+      message = "Hello World"
+      expected = {encryption: "keder ohulw", key: key, date: "040895"}
+
+      expect(enigma.encrypt(message, key)).to eq(expected)
+    end
   end
+
 end

8631743685ee5791fc810f56b826b51e640a65bc
Aliya Merali <merali4@gmail.com>
2021-04-24 17:24:11 -0600

    Add working stubs to encrypt_from_file test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -105,7 +105,7 @@ RSpec.describe Enigma do
 
   describe 'Shiftable module methods' do
     enigma = Enigma.new
-    
+
     it '#get_offset takes in a date string and returns an offset' do
       date = "220421"
       expect(enigma.get_offset(date)).to eq("7241")

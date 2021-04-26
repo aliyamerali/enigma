7ea360d77d591284a7a642467ec6cffeb3f80fbd
Aliya Merali <merali4@gmail.com>
2021-04-26 13:50:58 -0600

    Move tests for bkwd_calculate_key into new Module

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -107,24 +107,6 @@ RSpec.describe Enigma do
     end
   end
 
-  describe '#bkwd_calculate_key returns a key based on a shift and date' do
-    enigma = Enigma.new
-
-    it 'returns key' do
-      shifts = {A: 14, B: 86%27, C: 32%27, D:8}
-      date = "291018"
-      expected = "08304"
-      expect(enigma.bkwd_calculate_key(shifts, date)).to eq(expected)
-    end
-
-    it 'returns key' do
-      shifts = enigma.bkwd_calculate_shifts("qqubqeyigfxmpjzh")#{A: 16, B: 5, C: 12, D:4}
-      date = "220421"
-      expected_1 = "90357"
-      expected_2 = "63084"
-      expect(enigma.bkwd_calculate_key(shifts, date)).to eq(expected_1).or eq(expected_2)
-    end
-  end
 
   describe 'Shiftable module methods' do
     enigma = Enigma.new
@@ -178,12 +160,26 @@ RSpec.describe Enigma do
   describe 'BkwdCalculatable Module methods' do
     enigma = Enigma.new
 
-    it '#bkwd_calculate_shifts returns the shifts based on cyphertxt and date' do
-      cyphertext = "vjqtbeaweqihssi"
-      expected = {A: 14, B: 86%27, C: 32%27, D:8}
+    # it '#bkwd_calculate_shifts returns the shifts based on cyphertxt and date' do
+    #   cyphertext = "vjqtbeaweqihssi"
+    #   expected = {A: 14, B: 86%27, C: 32%27, D:8}
+    #
+    #   expect(enigma.bkwd_calculate_shifts(cyphertext)).to eq(expected)
+    # end
 
-      expect(enigma.bkwd_calculate_shifts(cyphertext)).to eq(expected)
+    it '#bkwd_calculate_key returns a key based on a shift and date' do
+      shifts = {A: 14, B: 86%27, C: 32%27, D:8}
+      date = "291018"
+      expected = "08304"
+      expect(enigma.bkwd_calculate_key(shifts, date)).to eq(expected)
+    end
+
+    it '#bkwd_calculate_key returns a key without a date entered' do
+      shifts = enigma.bkwd_calculate_shifts("qqubqeyigfxmpjzh")#{A: 16, B: 5, C: 12, D:4}
+      date = "220421"
+      expected_1 = "90357"
+      expected_2 = "63084"
+      expect(enigma.bkwd_calculate_key(shifts, date)).to eq(expected_1).or eq(expected_2)
     end
   end
-
 end
